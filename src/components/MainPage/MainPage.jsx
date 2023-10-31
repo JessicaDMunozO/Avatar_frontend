909d116b88624b92a1c0d109d7f30e9178f44db0
JessicaDMunozO <jessitae@gmail.com>
2023-10-31 00:02:19 -0500

    Checkbox criptomonedas y gráfica

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,9 +1,120 @@
 import React from 'react'
 
+import axios from 'axios'
+
+import '../../components/MainPage/MainPage.css'
+import '../../App.css'
+
+import { Bar } from 'react-chartjs-2'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+);
+
 const MainPage = () => {
-    return(
-        <div className='principalPage'>
-            <h1>Top 10 cryptos today</h1>
+    const baseURL = "http://localhost:4444/db/cryptos_last"
+    var data = ""
+
+    // Stored request response
+    const [post, setPost] = React.useState(null)
+
+    // GET request
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            data = response.data
+            setPost(data)
+        })
+    }, [])
+
+    if (!post) return null
+
+    // Checkbox per coin
+    const cryptos = post[0].cryptocurrencies.map((coin) => (
+        <label class="checkbox-label">
+            <input type="checkbox" class="checkbox" />
+            <div class="svg-icon">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 448 512"
+                >
+                    <path
+                        d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"
+                    ></path>
+                </svg>
+            </div>
+            <span class="containerCheckbox"></span>
+            {coin.name}
+        </label>
+    ))
+
+    // Array with cryptos name
+    const cryptos_name = post[0].cryptocurrencies.map((coin) => (coin.name))
+
+    // Array with cryptos values
+    const cryptos_value = post[0].cryptocurrencies.map((coin) => (coin.value))
+
+    // Bars chart
+    var my_data = {
+        labels: cryptos_name,
+        datasets: [
+            {
+                label: 'Price',
+                data: cryptos_value,
+                backgroundColor: 'rgba(0, 40, 143, 1)',
+            }
+        ]
+    }
+
+    var my_options = {
+        responsive: true,
+        animation: false,
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            y: {
+                min: 0,
+                max: 40000,
+                ticks: { color: 'rgb(0, 40, 143, 1)' }
+            },
+            x: {
+                ticks: { color: 'rgb(0, 40, 143, 1)' }
+            }
+        }
+    }
+
+    return (
+        <div className='component-specific'>
+            <div className='title'>
+                <h1>Top 10 cryptos today</h1>
+            </div>
+            <div className='cryptos'>
+                {cryptos}
+            </div>
+            <div className="chart">
+                <Bar data={my_data} options={my_options}></Bar>
+            </div>
         </div>
     )
 }
